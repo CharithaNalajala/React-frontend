000eccf984502bae4cf58841fa4962cef9d53116
CharithaNalajala <nalajalanalini879@gmail.com>
2024-08-11 19:09:00 +0530

    front

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+// import { Personal } from './components/resume/presonal';
+// import { Education } from './components/resume/education';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { SignIn } from './Components/signin/signin';
+import { SignUp } from './Components/signup/signup';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<>
+<BrowserRouter>
+<Routes>
+  <Route path="/signin" element={<SignIn />} />
+  <Route path="/signup" element={<SignUp />} />
+</Routes>
+
+</BrowserRouter>
+{/* <SignIn/>
+<SignUp/>
+    {/* < UseStateFunction/> */}
+    {/* <Personal/> */}
+    {/* <Education/> */} 
+</>
   );
 }
 
